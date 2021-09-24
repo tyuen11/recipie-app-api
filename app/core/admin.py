7f655183a3093d43cd4044c37618da32260490f8
Tim <timothy.yuen11@gmail.com>
2021-09-23 23:07:11 -0400

    fixed pep errors

--- app/core/admin.py
+++ app/core/admin.py
@@ -19,9 +19,10 @@ class UserAdmin(BaseUserAdmin):
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide', ),
+            'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
